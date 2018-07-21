c7de8bed2c9acd7166fafd414eaf4e4d79e7878b
VERCHERE Remi <r3verche@enib.fr>
2018-07-21 17:58:23 +0200

    ajout de l'emplacement des monuments

--- src/app/statistiques/statistiques.component.ts
+++ src/app/statistiques/statistiques.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit,OnDestroy } from '@angular/core';
 import {InfosMonumenttService} from '../services/infos-monumentt.service';
-import {D3Service,
-  D3,
-  Selection,
-  Axis,
-  BrushBehavior,
-  BrushSelection,
-  D3BrushEvent,
-  ScaleLinear,
-  ScaleOrdinal,
-  Transition} from 'd3-ng2-service';
+import {D3Service,D3,Selection} from 'd3-ng2-service';
+import {Map,View,Feature} from 'ol';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
+import OSM from 'ol/source/OSM';
+import {fromLonLat} from 'ol/proj.js';
+import { Point } from "ol/geom";
+import { Icon,Style } from "ol/style";
+import VectorSource from 'ol/source/Vector.js';
 
 @Component({
   selector: 'app-statistiques',
@@ -28,7 +26,6 @@ export class StatistiquesComponent implements OnInit {
       "nom":"Tour Eiffel",
       "description":"Tour de fer puddlé construite en 1887. Symbole de la capitale française, elle est le monument payant le plus visité du monde.",
       "hauteur":324,
-      "largeur":124.9,
       "pays":"France"
     }
   ];
@@ -54,7 +51,7 @@ export class StatistiquesComponent implements OnInit {
     .subscribe(data=>{
       this.evol=data;
       this.DrawCharHeight();
-      this.DrawCharWidth();
+      this.DrawMap();
     });
   }
 
@@ -69,7 +66,7 @@ export class StatistiquesComponent implements OnInit {
     let svg = this.d3.select('#hauteur')
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
-      .style('background-color', '#efefef');
+      .style('background-color', 'white');
 
     let chart = svg.append("g")
       .attr('class', 'bar')
@@ -103,7 +100,7 @@ export class StatistiquesComponent implements OnInit {
       .data(this.evol)
       .enter().append("rect")
       .attr("class", "bar")
-      .attr("fill","blue")
+      .attr("fill","#28a745")
       .attr("x", function(d) { return margin.left + x(d.nom) ; })
       .attr("width", x.bandwidth)
       .attr("y", function(d) { return y(d.hauteur); })
@@ -111,56 +108,54 @@ export class StatistiquesComponent implements OnInit {
   }
 
   /**
-   * Histogramme de comparaison des largeurs des monuments
+   *Dessin de la carte des monuments
    */
-
-  DrawCharWidth(){
-    let margin = {top: 5, right: 20, bottom: 30, left: 40};
-    let width = 600 - margin.left - margin.right;
-    let height = 600 - margin.top - margin.bottom;
- 
-    let svg = this.d3.select('#largeur')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom)
-      .style('background-color', '#efefef');
-
-    let chart = svg.append("g")
-      .attr('class', 'bar')
-      .attr('transform', `translate(${margin.left}, ${margin.top})`);
-
-    let xDomain = this.evol.map(d => d.nom);
-    let yDomain = [0, this.d3.max(this.evol, d=> d.largeur)];
-
-    let x = this.d3.scaleBand()
-            .domain(xDomain)
-            .rangeRound([0, width])
-            .padding(0.2);
-
-    let y = this.d3.scaleLinear()
-            .domain(yDomain)
-            .range([height, 0]);
-
-    // add the x Axis
-    svg.append("g")
-      .attr('class', 'x axis')
-      .attr('transform', `translate(${margin.left}, ${margin.top + height})`)
-      .call(this.d3.axisBottom(x));
-
-    // add the y Axis
-    svg.append("g")
-      .attr('class', 'y axis')
-      .attr('transform', `translate(${margin.left}, ${margin.top})`)
-      .call(this.d3.axisLeft(y));
-
-    svg.selectAll("bar")
-      .data(this.evol)
-      .enter().append("rect")
-      .attr("class", "bar")
-      .attr("fill","blue")
-      .attr("x", function(d) { return margin.left + x(d.nom) ; })
-      .attr("width", x.bandwidth)
-      .attr("y", function(d) { return y(d.largeur); })
-      .attr("height", function(d) { return height - y(d.largeur); });
+  DrawMap(){
+    var centerMap=fromLonLat([2.287592000000018,40.862725 ]);
+    var london = new Feature({
+      type: 'icon',
+      geometry: new Point(fromLonLat([-0.12755, 51.507222]))
+    });
+    var paris = new Feature({
+      type: 'icon',
+      geometry: new Point(fromLonLat([2.287592000000018, 48.862725]))
+    });
+    var gizeh = new Feature({
+      type: 'icon',
+      geometry: new Point(fromLonLat([31.132495500000005, 29.9772962]))
+    });
+    var style={
+      'icon': new Style({
+        image: new Icon({
+          anchor: [0.5, 1],
+          src: 'assets/marker.png'
+        })
+      }),
+    };
+    var vectorLayer = new VectorLayer({
+      source: new VectorSource({
+        features: [london, paris, gizeh]
+      }),
+      style: function(feature) {
+        
+        return style[feature.get('type')];
+      }
+    });
+    var map = new Map({
+      target: 'map',
+      layers: [
+        new TileLayer({
+          source: new OSM({
+            
+          })
+        }),
+        vectorLayer
+      ],
+      view: new View({
+        center: centerMap,
+        zoom: 4
+      })
+    });
   }
 
 }
